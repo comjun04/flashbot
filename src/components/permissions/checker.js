ecc51ebe4f072a91445a8c3b3248d54c638c01ba
My Name <comjun04@gmail.com>
2021-05-28 08:52:27 +0900

    refactor(tictactoe): move to component system

--- src/components/permissions/checker.js
+++ src/components/permissions/checker.js
@@ -1,10 +1,18 @@
-const canSendEmbed = (clientUser, channel) => {
+const canSendEmbed = (user, channel) => {
   if (channel.type === 'dm') return true
-  else if (channel.type !== 'text') return false
+  else if (channel.type !== 'text') return null
 
-  return channel.permissionsFor(clientUser).has('EMBED_LINKS')
+  return channel.permissionsFor(user).has('EMBED_LINKS')
+}
+
+const canManageMessages = (user, channel) => {
+  if (channel.type === 'dm') return false
+  else if (channel.type !== 'text') return null
+
+  return channel.permissionsFor(user).has('MANAGE_MESSAGES')
 }
 
 module.exports = {
-  canSendEmbed
+  canSendEmbed,
+  canManageMessages
 }
